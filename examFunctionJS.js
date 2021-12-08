8d35fd9bce71b88364ae826b98784d438dee6a4a
zhangjun <847003171@qq.com>
2021-12-08 10:01:38 +0800

    常用正则添加

--- examFunctionJS.js
+++ examFunctionJS.js
@@ -1,2 +1,3 @@
 require('./simpleFuncitonCollect')
-console.log(asyncSetTime(() => { console.log('say Hello!')}, 1000));
+// console.log(asyncSetTime(() => { console.log('say Hello!')}, 1000));
+// console.log(validateIsEmail('-0'));
--- simpleFuncitonCollect.js
+++ simpleFuncitonCollect.js
@@ -4,6 +4,24 @@ asyncSetTime = (funName, time) => {
         setTimeout(() => { funName() }, time);
     }
 }
+// 效验输入的是否是数字
+validateIsNumber = (number) => {
+    let numPattern = /^-?\d*\.?\d+$/;
+    return numPattern.test(number)
+}
+// 效验输入的是否是邮箱
+validateIsEmail = (number) => {
+    let numPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    return numPattern.test(number)
+}
+// 效验输入的是否是手机号码
+validateIsEmail = (number) => {
+    let numPattern = /^1[34578]\d{9}$/;
+    return numPattern.test(number)
+}
+//提取Url里的参数
 module.exports = {
-    asyncSetTime
+    asyncSetTime,
+    validateIsNumber,
+    validateIsEmail
 }
